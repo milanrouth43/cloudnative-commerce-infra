1ea79f9c45b2171132826ef1a1d439ec1ccf2887
Ubuntu <ubuntu@ip-172-31-17-55.ec2.internal>
2026-04-13 09:03:09 +0000

    fix: update aws provider version

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 
   cloud {
-    organization = "platform-engineering-demo-milan"   # 👈 YOUR ORG NAME
+    organization = "platform-engineering-demo-milan"   
     workspaces {
       name = "cnc-logging-dev"
     }
